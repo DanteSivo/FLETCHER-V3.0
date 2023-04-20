795ea1dd0fe80c5e39fadfca408fffdf8480ef8d
Dante Sivo <dvs8452@g.rit.edu>
2023-04-19 21:20:30 -0400

    Bluetooth adjustable parameters

--- RIT-Launch-LoRa-Tester/ESP32-BaseKit/ESP32-BaseKit.ino
+++ RIT-Launch-LoRa-Tester/ESP32-BaseKit/ESP32-BaseKit.ino
@@ -12,17 +12,42 @@
 #define rst 14
 #define dio0 2
 
+#define BLE true
+
 #if !defined(CONFIG_BT_ENABLED) || !defined(CONFIG_BLUEDROID_ENABLED)
 #error Bluetooth is not enabled! Please run `make menuconfig` to and enable it
 #endif
 
+String command;
+String var;
+String val;
+
+char buffer[7];
+int PW = 0; // current TX-PW rating
+int SF = 12; // current SF
+long BW = 150E3; // current BW
+int LG = 6; // LNA Gain
+
 BluetoothSerial SerialBT;
+
+void print(String msg){
+  Serial.print(msg);
+  if (BLE)
+    SerialBT.print(msg);
+}
+
+void println(String msg){
+  Serial.println(msg);
+  if (BLE)
+    SerialBT.println(msg);
+}
 
 void setup() {
   //initialize Serial Monitor
   Serial.begin(115200);
+  SerialBT.begin("RIT-BaseKit-RX"); //Bluetooth device name
   while (!Serial);
-  Serial.println("LoRa Receiver");
+  println("LoRa Receiver");
 
   //setup LoRa transceiver module
   LoRa.setPins(ss, rst, dio0);
@@ -32,42 +57,112 @@ void setup() {
   //866E6 for Europe
   //915E6 for North America
   while (!LoRa.begin(915E6)) {
-    Serial.println(".");
+    println(".");
     delay(500);
   }
 
-  LoRa.setSpreadingFactor(12);
-  LoRa.setSignalBandwidth(125E3);
-  
+  LoRa.setSpreadingFactor(SF);
+  LoRa.setSignalBandwidth(BW);
+  LoRa.setGain(LG);
    // Change sync word (0xF3) to match the receiver
   // The sync word assures you don't get LoRa messages from other LoRa transceivers
   // ranges from 0-0xFF
   LoRa.setSyncWord(0xF3);
   LoRa.dumpRegisters(Serial);
   LoRa.dumpRegisters(SerialBT);
-  Serial.println("LoRa Initializing OK!");
-  SerialBT.println("LoRa Initializing OK!");
-  SerialBT.begin("RIT-BaseKit-RX"); //Bluetooth device name
+  println("LoRa Initializing OK!");
+}
+
+void updateRegisters(Stream& in) {
+    command = in.readStringUntil('\n');
+    command.trim();
+    var = command.substring(0,2);
+    val = command.substring(3);
+   
+    print("Command: ");
+    println(command);
+    
+    print("Got Command: ");
+    println(var);
+    
+    print("Got Value: ");
+    println(val);
+
+    if(var.equals("PW")){ // Adjust TX Power
+      print("PW=");
+      itoa(PW, buffer, 10);
+      print(buffer);
+      print(" is now ");
+      println(val);
+      PW = val.toInt();
+    } else if(var.equals("SF")){ // Adjust Spreading Factor
+      print("SF=");
+      itoa(SF, buffer, 10);
+      print(buffer);
+      print(" is now ");
+      println(val);
+      SF = val.toInt();
+      LoRa.setSpreadingFactor(SF);
+    } else if(var.equals("BW")){   // Adjust Bandwidth
+      print("BW=");
+      ltoa(BW, buffer, 10);
+      print(buffer);
+      print(" is now ");
+      println(val);
+      BW = val.toInt();
+      LoRa.setSignalBandwidth(BW);
+    } else if (var.equals("LG")) {
+      print("LG=");
+      itoa(LG, buffer, 10);
+      print(buffer);
+      print(" is now ");
+      println(val);
+      LG = val.toInt();
+      LoRa.setGain(LG);
+    } else if (var.equals("RR")) { // Dump Register contents
+      println("Dumpuing register contents");
+      LoRa.dumpRegisters(Serial);
+      LoRa.dumpRegisters(SerialBT);
+      print("BW=");
+      ltoa(BW, buffer, 10);
+      print(buffer);
+      print(" SF=");
+      itoa(SF, buffer, 10);
+      print(buffer);
+      print(" LG=");
+      itoa(LG, buffer, 10);
+      print(buffer);
+      print(" PW=");
+      itoa(PW, buffer, 10);
+      println(buffer);
+    }
+      else {
+      Serial.println("Command not recognizied");
+    }
 }
 
 void loop() {
-  // try to parse packet
-  int packetSize = LoRa.parsePacket();
-  if (packetSize) {
-    // received a packet
-    Serial.print("Received packet '");
-
-    // read packet
-    while (LoRa.available()) {
-      String LoRaData = LoRa.readString();
-      Serial.print(LoRaData); 
-      SerialBT.print(LoRaData);
+    if (Serial.available()) {
+      updateRegisters(Serial);
+    } else if (SerialBT.available()) {
+       updateRegisters(SerialBT);
     }
-
-    // print RSSI of packet
-    Serial.print("' with RSSI ");
-    SerialBT.print("' with RSSI ");
-    Serial.println(LoRa.packetRssi());
-    SerialBT.println(LoRa.packetRssi());
+    else {
+      // try to parse packet
+      int packetSize = LoRa.parsePacket();
+      if (packetSize) {
+        // received a packet
+        Serial.print("Received packet '");
+    
+        // read packet
+        while (LoRa.available()) {
+          String LoRaData = LoRa.readString();
+          print(LoRaData); 
+        }
+    
+        // print RSSI of packet
+        print("' with RSSI ");
+        println(itoa(LoRa.packetRssi(), buffer, 10));
+      }
   }
 }
--- RIT-Launch-LoRa-Tester/ESP32-FieldKit/ESP32-FieldKit.ino
+++ RIT-Launch-LoRa-Tester/ESP32-FieldKit/ESP32-FieldKit.ino
@@ -37,6 +37,19 @@
 #error Bluetooth is not enabled! Please run `make menuconfig` to and enable it
 #endif
 
+String command;
+String var;
+String val;
+
+char buffer[7];
+// EDIT ME
+#define BLE true
+
+int PW = 20; // current TX-PW rating
+int SF = 12; // current SF
+long BW = 150E3; // current BW
+int LG = 6; // LNA Gain
+// END EDIT ME
 BluetoothSerial SerialBT;
 
 #include <SparkFun_u-blox_GNSS_Arduino_Library.h> //http://librarymanager/All#SparkFun_u-blox_GNSS
@@ -47,8 +60,22 @@ SFE_UBLOX_GNSS myGNSS;
 #define rst 14
 #define dio0 2
 
+
+
 long lastTime = 0; //Simple local timer. Limits amount if I2C traffic to u-blox module.
 int counter = 0;
+
+void print(String msg){
+  Serial.print(msg);
+  if (BLE)
+    SerialBT.print(msg);
+}
+
+void println(String msg){
+  Serial.println(msg);
+  if (BLE)
+    SerialBT.println(msg);
+}
 
 void setup()
 {
@@ -83,9 +110,10 @@ void setup()
     delay(500);
   }
 
-  LoRa.setSpreadingFactor(12);
-  LoRa.setSignalBandwidth(125E3);
-  LoRa.setTxPower(23);
+  LoRa.setSpreadingFactor(SF);
+  LoRa.setSignalBandwidth(BW);
+  LoRa.setTxPower(PW);
+  LoRa.setGain(LG);
    // Change sync word (0xF3) to match the receiver
   // The sync word assures you don't get LoRa messages from other LoRa transceivers
   // ranges from 0-0xFF
@@ -96,85 +124,158 @@ void setup()
   SerialBT.println("LoRa Initializing OK!");
 }
 
+void updateRegisters(Stream& in) {
+    command = in.readStringUntil('\n');
+    command.trim();
+    var = command.substring(0,2);
+    val = command.substring(3);
+   
+    print("Command: ");
+    println(command);
+    
+    print("Got Command: ");
+    println(var);
+    
+    print("Got Value: ");
+    println(val);
+
+    if(var.equals("PW")){ // Adjust TX Power
+      print("PW=");
+      itoa(PW, buffer, 10);
+      print(buffer);
+      print(" is now ");
+      println(val);
+      PW = val.toInt();
+    } else if(var.equals("SF")){ // Adjust Spreading Factor
+      print("SF=");
+      itoa(SF, buffer, 10);
+      print(buffer);
+      print(" is now ");
+      println(val);
+      SF = val.toInt();
+      LoRa.setSpreadingFactor(SF);
+    } else if(var.equals("BW")){   // Adjust Bandwidth
+      print("BW=");
+      ltoa(BW, buffer, 10);
+      print(buffer);
+      print(" is now ");
+      println(val);
+      BW = val.toInt();
+      LoRa.setSignalBandwidth(BW);
+    } else if (var.equals("LG")) {
+      print("LG=");
+      itoa(LG, buffer, 10);
+      print(buffer);
+      print(" is now ");
+      println(val);
+      LG = val.toInt();
+      LoRa.setGain(LG);
+    } else if (var.equals("RR")) { // Dump Register contents
+      println("Dumpuing register contents");
+      LoRa.dumpRegisters(Serial);
+      LoRa.dumpRegisters(SerialBT);
+      print("BW=");
+      ltoa(BW, buffer, 10);
+      print(buffer);
+      print(" SF=");
+      itoa(SF, buffer, 10);
+      print(buffer);
+      print(" LG=");
+      itoa(LG, buffer, 10);
+      print(buffer);
+      print(" PW=");
+      itoa(PW, buffer, 10);
+      println(buffer);
+    }
+      else {
+      Serial.println("Command not recognizied");
+    }
+}
+
 void loop()
 {
-  //Query module only every second. Doing it more often will just cause I2C traffic.
-  //The module only responds when a new position is available
-  if (millis() - lastTime > 1000)
-  {
-    LoRa.beginPacket();
+  if (Serial.available()) {
+      updateRegisters(Serial);
+  } else if (SerialBT.available()) {
+     updateRegisters(SerialBT);
+  }
+  else {
+    //Query module only every second. Doing it more often will just cause I2C traffic.
+    //The module only responds when a new position is available
+    if (millis() - lastTime > 1000)
+    {
+      LoRa.beginPacket();
+      
+      lastTime = millis(); //Update the timer
     
-    lastTime = millis(); //Update the timer
-
-    uint8_t GMThour = myGNSS.getHour();
-    if (GMThour > 4) {
-      GMThour = GMThour - 4;
-      Serial.print(GMThour);
-      LoRa.print(GMThour);
-      SerialBT.print(GMThour);
-    } else {
-      GMThour = 24-(4-GMThour);
-      Serial.print(GMThour);
-      LoRa.print(GMThour);
-      SerialBT.print(GMThour);
+      uint8_t GMThour = myGNSS.getHour();
+      if (GMThour > 4) {
+        GMThour = GMThour - 4;
+        Serial.print(GMThour);
+        LoRa.print(GMThour);
+        SerialBT.print(GMThour);
+      } else {
+        GMThour = 24-(4-GMThour);
+        Serial.print(GMThour);
+        LoRa.print(GMThour);
+        SerialBT.print(GMThour);
+      }
+      Serial.print(":");
+      LoRa.print(":");
+      SerialBT.print(":");
+    
+      Serial.print(myGNSS.getMinute());
+      LoRa.print(myGNSS.getMinute());
+      SerialBT.print(myGNSS.getMinute());
+      Serial.print(":");
+      SerialBT.print(":");
+      LoRa.print(":");
+      
+      Serial.print(myGNSS.getSecond());
+      LoRa.print(myGNSS.getSecond());
+      SerialBT.print(myGNSS.getSecond());
+      Serial.print(":");
+      SerialBT.print(":");
+      LoRa.print(":");
+      
+      int32_t latitude = myGNSS.getLatitude();
+      Serial.print(F(" Lat: "));
+      Serial.print(latitude);
+      SerialBT.print(F(" Lat: "));
+      SerialBT.print(latitude);
+      LoRa.print(F(" Lat: "));
+      LoRa.print(latitude);
+    
+      int32_t longitude = myGNSS.getLongitude();
+      Serial.print(F(" Long: "));
+      Serial.println(longitude);
+      Serial.print(F(" (degrees * 10^-7)"));
+      LoRa.print(F(" Long: "));
+      LoRa.println(longitude);
+      LoRa.print(F(" (degrees * 10^-7)"));
+      SerialBT.print(F(" Long: "));
+      SerialBT.println(longitude);
+      SerialBT.print(F(" (degrees * 10^-7)"));
+    
+      int32_t altitude = myGNSS.getAltitude();
+      Serial.print(F(" Alt: "));
+      Serial.print(altitude);
+      Serial.print(F(" (mm)"));
+      
+    
+      uint8_t SIV = myGNSS.getSIV();
+      Serial.print(F(" SIV: "));
+      Serial.print(SIV);
+      LoRa.print(F(" SIV: "));
+      LoRa.print(SIV);
+      SerialBT.print(F(" SIV: "));
+      SerialBT.print(SIV);
+    
+      LoRa.endPacket();
+    
+      Serial.println();
+      SerialBT.println();
+      counter++;
     }
-    Serial.print(":");
-    LoRa.print(":");
-    SerialBT.print(":");
-
-    Serial.print(myGNSS.getMinute());
-    LoRa.print(myGNSS.getMinute());
-    SerialBT.print(myGNSS.getMinute());
-    Serial.print(":");
-    SerialBT.print(":");
-    LoRa.print(":");
-    
-    Serial.print(myGNSS.getSecond());
-    LoRa.print(myGNSS.getSecond());
-    SerialBT.print(myGNSS.getSecond());
-    Serial.print(":");
-    SerialBT.print(":");
-    LoRa.print(":");
-    
-    int32_t latitude = myGNSS.getLatitude();
-    Serial.print(F(" Lat: "));
-    Serial.print(latitude);
-    SerialBT.print(F(" Lat: "));
-    SerialBT.print(latitude);
-    LoRa.print(F(" Lat: "));
-    LoRa.print(latitude);
-
-    int32_t longitude = myGNSS.getLongitude();
-    Serial.print(F(" Long: "));
-    Serial.println(longitude);
-    Serial.print(F(" (degrees * 10^-7)"));
-    LoRa.print(F(" Long: "));
-    LoRa.println(longitude);
-    LoRa.print(F(" (degrees * 10^-7)"));
-    SerialBT.print(F(" Long: "));
-    SerialBT.println(longitude);
-    SerialBT.print(F(" (degrees * 10^-7)"));
-
-    int32_t altitude = myGNSS.getAltitude();
-    Serial.print(F(" Alt: "));
-    Serial.print(altitude);
-    Serial.print(F(" (meters)"));
-    
-
-    uint8_t SIV = myGNSS.getSIV();
-    Serial.print(F(" SIV: "));
-    Serial.print(SIV);
-    LoRa.print(F(" SIV: "));
-    LoRa.print(SIV);
-    SerialBT.print(F(" SIV: "));
-    SerialBT.print(SIV);
-
-    LoRa.endPacket();
-
-    Serial.println();
-    SerialBT.println();
-    counter++;
-
-    delay(250);
   }
 }
